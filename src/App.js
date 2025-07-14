1ad93c6ca4b923e126e838fd7637382318440306
Millenniumzz <sinbatzz5678_@hotmail.com>
2025-07-14 11:41:53 +0700

    create style.css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import './style.css';
 
 function App() {
-  const divStyle = {color:'red',backgroundColor:'powderBlue',fontSize:'larger',padding:'3px',}
-  return <div style={divStyle}>Hello React</div>
+  return (
+    <>
+    <h2>DeveloperThai.com</h2>
+    <div className='title'>React & React Native</div>
+    </>
+  )
 }
 
 export default App;
